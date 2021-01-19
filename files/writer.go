38e35713809715918a58486554a54515ad6e1665
JAGLees <james.lees@gmail.com>
2021-01-19 12:10:54 +0000

    Removed async code to start rewriting as sync function.

--- files/writer.go
+++ files/writer.go
@@ -1,16 +1,5 @@
 package files
 
 import (
-  log "github.com/sirupsen/logrus"
-  "sync"
+  // log "github.com/sirupsen/logrus"
 )
-
-func WriteLines(fileName string, c chan string, wg *sync.WaitGroup) error {
-  log.Debug("files WriteLines: Writing lines from channel to file")
-  for i := range c {
-		log.Debug(i)
-	}
-  log.Debug("files WriteLines: Read")
-  wg.Done()
-  return nil
-}
